2783ee29b5f439704224c70e365ee8909019b6b1
Brenten L. Canfield <brentenlavoy@gmail.com>
2022-06-14 12:33:26 -0600

    App instance specific SQL written for over_time_line_chart Shiny module.

--- inst/sql/student_term_course_section.sql
+++ inst/sql/student_term_course_section.sql
@@ -7,7 +7,7 @@
 - student_college
 - student_program
 - student_full_name
-- course_section_gpa
+- course_section_grade_points
 - course_section_description
 
 With the following filter:
@@ -15,3 +15,47 @@ WHERE is_student_athlete
 
 This filter will reduce the amount of data pulled, greatly.
 */
+SELECT a.student_id AS student_id,
+       a.term_id AS term_id,
+       b.course_id AS course_section_id,
+
+       -- student information
+       a.primary_major_college_desc AS student_college,
+       a.primary_program_desc AS student_program,
+       c.last_name || ', ' || c.first_name AS student_full_name,
+       COALESCE(f.has_accepted_scholarship, FALSE) AS student_has_accepted_scholarship,
+       COALESCE(f.scholarship_amount_accepted, 0) AS student_scholarship_amount_accepted,
+       -- TODO: Need student_team HERE
+       -- TODO: Data for this should be in data warehouse.
+
+       -- course section information
+       b.grade_points AS course_section_grade_points,
+       d.course_desc AS course_section_description
+
+FROM export.student_term_level a
+LEFT JOIN export.student_section b
+       ON a.student_id = b.student_id
+      AND a.term_id = b.term_id
+LEFT JOIN export.student c
+       ON a.student_id = c.student_id
+LEFT JOIN export.course d
+       ON b.course_id = d.course_id
+
+/* BEGIN Athletic Scholarship SQL logic  */
+-- TODO: this SQL logic should be in data warehouse
+LEFT JOIN (SELECT DISTINCT f1.student_id,
+                   f1.term_id,
+                   SUM(f1.amount_accepted) AS scholarship_amount_accepted,
+                   TRUE AS has_accepted_scholarship
+            FROM export.student_financial_aid_year_fund_term_detail f1
+            -- Student has accepted the scholarship.
+            WHERE f1.amount_accepted > 0
+            -- The type of fund is a scholarship.
+            AND f1.is_scholarship
+            GROUP BY f1.student_id, f1.term_id) f
+    ON a.student_id = f.student_id
+    AND a.term_id = f.term_id
+/* END Athletic Scholarship SQL logic  */
+
+-- only pull information for student athletes
+WHERE a.is_athlete;
